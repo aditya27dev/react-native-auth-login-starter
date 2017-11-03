9ece2dec6b76ea1754634fc4094a6c0338cec84b
MrPeproni <adityabatra277@gmail.com>
2017-11-03 09:58:05 +0530

    Added iOS Support

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Text, TouchableNativeFeedback, View, TextInput } from 'react-native';
+import { Platform, Text, TouchableNativeFeedback, View, TextInput, TouchableHighlight } from 'react-native';
 import { scale, verticalScale } from './scaling';
 
 const Button = ({ backgroundColor, text, onChangeText }) => {
-    return (
-        <TouchableNativeFeedback
-        onPress={onChangeText}
-        >
-            <View style={{paddingVertical: 8, paddingHorizontal: 30, backgroundColor: backgroundColor ? backgroundColor : '#0093dd', borderRadius: 4}}>
-                <Text style={{color: '#fff'}}>{text}</Text>
-            </View>
-        </TouchableNativeFeedback>
-    );
+    if(Platform.OS == 'android'){
+        return (
+            <TouchableNativeFeedback
+            onPress={onChangeText}
+            >
+                <View style={{paddingVertical: 8, paddingHorizontal: 30, backgroundColor: backgroundColor ? backgroundColor : '#0093dd', borderRadius: 4}}>
+                    <Text style={{color: '#fff'}}>{text}</Text>
+                </View>
+            </TouchableNativeFeedback>
+        );
+    }
+    if(Platform.OS == 'ios'){
+        return (
+            <TouchableHighlight
+            onPress={onChangeText}
+            >
+                <View style={{paddingVertical: 8, paddingHorizontal: 30, backgroundColor: backgroundColor ? backgroundColor : '#0093dd', borderRadius: 4}}>
+                    <Text style={{color: '#fff'}}>{text}</Text>
+                </View>
+            </TouchableHighlight>
+        );
+    }
 };
 export { Button };
--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,22 +1,49 @@
 import React from 'react';
-import { Text, TouchableOpacity, View, TextInput } from 'react-native';
+import { Platform, StyleSheet, Text, TouchableOpacity, View, TextInput } from 'react-native';
 import { scale, verticalScale } from './scaling';
 
+const styles = StyleSheet.create({
+    iosView: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#d3d3d3',
+        paddingBottom: 5
+    }
+})
+
 const InputField = ({ label, value, onChangeText, multiline, placeholder, placeholderTextColor, inputStyle, underlineColor }) => {
-    return (
-        <View style={{width: '90%'}}>
-            <Text style={{color: '#0093dd', marginLeft: 4, fontSize: 16}}>{label}</Text>
-            <TextInput
-                placeholder={placeholder}
-                placeholderTextColor={placeholderTextColor}
-                style={inputStyle}
-                value={value}
-                onChangeText={onChangeText}
-                underlineColorAndroid={underlineColor}
-                multiline={multiline ? multiline : false}
-                autoCapitalize="sentences"
-            />
-        </View>
-    );
+    let textInputField = inputStyle ? inputStyle : textInputField;
+    if(Platform.OS == 'android'){
+        return (
+            <View style={{width: '90%'}}>
+                <Text style={{color: '#0093dd', marginLeft: 4, fontSize: 16}}>{label}</Text>
+                <TextInput
+                    placeholder={placeholder}
+                    placeholderTextColor={placeholderTextColor}
+                    style={textInputField}
+                    value={value}
+                    onChangeText={onChangeText}
+                    underlineColorAndroid={underlineColor}
+                    multiline={multiline ? multiline : false}
+                    autoCapitalize="sentences"
+                />
+            </View>
+        );
+    }
+    if(Platform.OS == 'ios'){
+        return (
+            <View style={[styles.iosView, {width: '90%'}]}>
+                <Text style={{color: '#0093dd', fontSize: 16, marginVertical: 12}}>{label}</Text>
+                <TextInput
+                    placeholderTextColor={placeholderTextColor}
+                    style={textInputField}
+                    value={value}
+                    onChangeText={onChangeText}
+                    underlineColorAndroid={underlineColor}
+                    multiline={multiline ? multiline : false}
+                    autoCapitalize="sentences"
+                />
+            </View>
+        );
+    }
 };
 export { InputField };
